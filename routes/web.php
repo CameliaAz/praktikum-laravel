d9a1e0178a9e162dd13062f7f8bf435140c1e164
CameliaAz <cameliaazzahra94@gmail.com>
2024-11-30 14:54:41 +0700

    hasil praktikum-4

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,12 @@
 <?php
 
-use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
 
 
-Route::get('/', [WelcomeController::class, 'index']);
-
-Route::group(['prefix' => 'user'], function () { // menampilkan halaman awal user
-    Route::get('/', [UserController::class, 'index']); // menampilkan data user dalam bentuk json untuk datatables
-    Route::post('/list', [UserController::class, 'list']); // menampilkan data user dalam bentuk json untuk datatables
-    Route::get('/create', [UserController::class, 'create']); // menampilkan halaman form tambah user
-    Route::post('/', [UserController::class, 'store']); // menyimpan data user baru
-    Route::get('/{id}', [UserController::class, 'show']); // menampilkan detail user
-    Route::get('/{id}/edit', [UserController::class, 'edit']); // menampilkan halaman form edit user
-    Route::put('/{id}', [UserController::class, 'update']); // menyimpan perubahan data user
-    Route::delete('/{id}', [UserController::class, 'destroy']); // menghapus data user
+Route::get('/', function () {
+return view('welcome');
 });
+
+Route::get('/level', [LevelController::class, 'index']);
+?>
