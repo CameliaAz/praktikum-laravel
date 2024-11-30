48e14903de98ed35daf6772c087726fc827e6cc0
CameliaAz <cameliaazzahra94@gmail.com>
2024-11-30 23:41:30 +0700

    hasil praktikum-5

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -12,13 +12,13 @@ class UserModel extends Model
 {
     use HasFactory;
 
-    protected $table = 'm_user';
-    protected $primaryKey = 'user_id';
+    protected $table = 'm_user'; //mendefinisikan nama tabel yang digunakan oleh model ini
+    protected $primaryKey = 'user_id'; //mendefinisikan primary key dari tabel yang digunakan
 
-    protected $fillable = ['user_id','level_id', 'username', 'nama', 'password'];
+    // protected $fillable = ['user_id','level_id', 'username', 'nama', 'password'];
 
-    public function level(): BelongsTo
-    {
-        return $this->belongsTo(LevelModel::class, 'level_id');
-    }
+    // public function level(): BelongsTo
+    // {
+    //     return $this->belongsTo(LevelModel::class, 'level_id');
+    // }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
 
@@ -9,4 +10,5 @@ return view('welcome');
 });
 
 Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
 ?>
